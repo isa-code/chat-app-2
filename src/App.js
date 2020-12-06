dafbc6c0155733001b61720ee492c047d26a7ac3
Guilherme isabelle <isabelle.guilherme@csenen.com>
2020-12-06 11:38:12 +0100

    création des div et du composant (enclosing tag)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Contact from './components/Contact';
-import './App.css';
+
 
 function App() {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Contact.css';
 
-const online = true;
-
 function Contact() {
     return (
+
         <div className="Contact">
             <img className="avatar"
-                src="https://randomuser.me/api/portraits/women/19.jpg" 
-                alt="avatar"
+                src="https://randomuser.me/api/portraits/women/19.jpg"
+                alt="ID photo"
             />
-            <p className="name">
-                Madison Stanley
-            </p>
+            <div>
+                <div className="name">
+                    Madison Stanley
+                </div>
 
-            <article className="status">
-                <p className="status-text">Online</p>
-                <p className="status-online"></p>
-            </article>
+                <div className="status">
+                    <p className="status-online"></p>
+                    <a className="status-text">Online</a>
+                </div>
+            </div>
         </div>
+
     )
 }
 
